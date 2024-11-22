a1fcc0b20a441e747f4feedc26ceacb86aa66bd1
WFCCS <williamsilva01032002@gmail.com>
2024-11-22 10:50:53 -0300

    Delete TCC directory

--- TCC/Sites/script.js
+++
@@ -1,9 +0,0 @@
-// Captura o formulário pelo ID
-const form = document.getElementById('contactForm');
-
-// Adiciona um evento de submissão ao formulário
-form.addEventListener('submit', function (event) {
-    event.preventDefault(); // Impede o envio real para demonstrar a mensagem
-    alert('Sua mensagem foi enviada com sucesso! Entraremos em contato em breve.'); // Exibe o aviso
-    form.reset(); // Limpa os campos do formulário
-});
